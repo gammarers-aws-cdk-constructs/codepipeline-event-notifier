7b78d74192f6d8e411068b3fd8930ec971b1c702
yicr <yicr@users.noreply.github.com>
2026-07-07 04:03:46 +0000

    chore: project created with projen

---
+++ .projenrc.ts
@@ -0,0 +1,57 @@
+import { awscdk, javascript, github } from 'projen';
+const project = new awscdk.AwsCdkConstructLibrary({
+  author: 'yicr',
+  authorAddress: 'yicr@users.noreply.github.com',
+  cdkVersion: '2.232.0',
+  typescriptVersion: '6.0.x',
+  jsiiVersion: '6.0.x',
+  name: 'workspace',
+  packageManager: javascript.NodePackageManager.NPM,
+  projenrcTs: true,
+  repositoryUrl: 'https://github.com/gammarers-aws-cdk-constructs/codepipeline-event-notifier.git',
+  releaseToNpm: false,
+  npmTrustedPublishing: false,
+  npmAccess: javascript.NpmAccess.PUBLIC,
+  minNodeVersion: '20.0.0',
+  workflowNodeVersion: '24.x',
+  depsUpgradeOptions: {
+    workflowOptions: {
+      labels: ['auto-approve', 'auto-merge'],
+      schedule: javascript.UpgradeDependenciesSchedule.NEVER,
+    },
+  },
+  githubOptions: {
+    projenCredentials: github.GithubCredentials.fromApp({
+      permissions: {
+        pullRequests: github.workflows.AppPermission.WRITE,
+        contents: github.workflows.AppPermission.WRITE,
+        workflows: github.workflows.AppPermission.WRITE,
+      },
+    }),
+  },
+  autoApproveOptions: {
+    allowedUsernames: [
+      'gammarers-projen-upgrade-bot[bot]',
+      'yicr',
+    ],
+  },
+  jestOptions: {
+    extraCliOptions: ['--silent'],
+  },
+  tsconfigDev: {
+    compilerOptions: {
+      strict: true,
+    },
+  },
+  lambdaOptions: {
+    // target node.js runtime
+    runtime: awscdk.LambdaRuntime.NODEJS_24_X,
+    bundlingOptions: {
+      // list of node modules to exclude from the bundle
+      externals: ['@aws-sdk/*'],
+      sourcemap: true,
+    },
+  },
+});
+project.addPackageIgnore('/.devcontainer');
+project.synth();
---
+++ README.md
@@ -0,0 +1,1 @@
+# CodePipeline Event Notifier
